8990c9a84f0549bd7a5670f1071953e95cd3b704
SukeshP1995 <sukeshpabolu@gmail.com>
2019-02-28 13:49:10 +0530

    second commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('dist'));
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
